d6f4372780b59c422bd72d3eba29bc9ad2639cb3
Lucas H. Xu <xuh@bu.edu>
2017-03-09 14:53:21 -0500

    Add new instance action:Resize

--- troposphere/static/js/models/InstanceActionRequest.js
+++ troposphere/static/js/models/InstanceActionRequest.js
@@ -7,26 +7,18 @@ export default Backbone.Model.extend({
         var instance = attrs.instance;
         if (!instance)
             throw new Error("Missing instance");
-        if (!instance.get("provider").uuid)
-            throw new Error("Missing instance.provider.uuid");
-        if (!instance.get("identity").uuid)
-            throw new Error("Missing instance.identity.uuid");
         if (!instance.get("uuid"))
             throw new Error("Missing instance.uuid");
     },
 
     url: function() {
         var instance = this.get("instance"),
-            instanceId = instance.get("uuid"),
-            providerId = instance.get("provider").uuid,
-            identityId = instance.get("identity").uuid;
+            instanceId = instance.get("uuid");
 
         return (
-        globals.API_ROOT +
-        "/provider/" + providerId +
-        "/identity/" + identityId +
+        globals.API_V2_ROOT +
         "/instance/" + instanceId +
-        "/action"
+        "/actions"
         )
     }
 });
