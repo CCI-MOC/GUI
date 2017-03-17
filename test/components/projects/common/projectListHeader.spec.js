962cca53b34aa480c93ea159a486edfc9be9597a
xuhang57 <xuh@bu.edu>
2017-03-17 17:57:41 -0400

    add a working test case

--- test/components/projects/common/projectListHeader.spec.js
+++ test/components/projects/common/projectListHeader.spec.js
@@ -1,33 +1,17 @@
-// 'react',
-// 'backbone',
-// '',
-// 'test/fixtures/projects.fixture'
-// ], function(React, Backbone, ProjectListHeader, projectCollectionFixture) {
-
-
-
-var TestUtils = require("react-addons-test-utils"),
-    projectListHeader = require("components/projects/common/ProjectListHeader"),
-    projectCollectionFixture = require("./test/fixtures/projects.fixture"),
-    title;
+import React from 'react';
+import {shallow} from 'enzyme';
 
 describe("Project List Header", function() {
-
-    beforeEach(function() {
-        title = projectCollectionFixture.length + " Projects";
+    var ReactTestUtils = require('react-addons-test-utils'),
+        ProjectListHeader = React.createFactory(require("/opt/dev/troposphere/troposphere/static/js/components/projects/common/ProjectListHeader")),
+        title;
+    it("should display the provided title", function() {
+        title = "3" + "Projects"
         var header = ProjectListHeader({
             title: title
         });
-        projectListHeader = TestUtils.renderIntoDocument(header);
+        ProjectListHeader = ReactTestUtils.renderIntoDocument(header);
+        var h1 = ReactTestUtils.findRenderedDOMComponentWithTag(ProjectListHeader, "h1");
+        expect(h1.textContent).toBe(title);
     });
-
-    it("should display the provided title", function() {
-        var h1 = TestUtils.findRenderedDOMComponentWithTag(projectListHeader, "h1");
-        var node = h1.getDOMNode();
-        expect(node.textContent).toBe(title);
-    });
-
-
 });
-
-// });
