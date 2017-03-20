3264b6d5421c4169a20c1d1f99c1f5b182f247fe
xuhang57 <xuh@bu.edu>
2017-03-20 16:50:31 -0400

    fix the imported file directory

--- test/components/projects/common/projectListHeader.spec.js
+++ test/components/projects/common/projectListHeader.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 describe("Project List Header", function() {
     var ReactTestUtils = require('react-addons-test-utils'),
-        ProjectListHeader = React.createFactory(require("/opt/dev/troposphere/troposphere/static/js/components/projects/common/ProjectListHeader")),
+        ProjectListHeader = React.createFactory(require("components/projects/common/ProjectListHeader")),
         title;
     it("should display the provided title", function() {
         title = "3" + "Projects"
