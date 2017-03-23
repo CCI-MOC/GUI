ab26a9a2e794ea668270cc3ebf3023786d9e7930
Lucas H. Xu <xuh@bu.edu>
2017-03-23 16:22:08 -0400

    fix ci probs

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -2,16 +2,5 @@ import "babel-polyfill";
 import bootstrapper from "bootstrapper";
 import "css/app/app.scss";
 
-<<<<<<< HEAD
-=======
 
-if(!window.SENTRY_ENABLED) {
-    let sentryDSN = window.SENTRY_DSN;
-    Raven.config(sentryDSN, {
-        release: window.SENTRY_RELEASE
-    }).install();
-}
-
-
->>>>>>> a856735a7697a36e433d85ee5294bc58ce0f0d94
 bootstrapper.run();
--- troposphere/static/js/public_site/main.js
+++ troposphere/static/js/public_site/main.js
@@ -1,15 +1,5 @@
 import "babel-polyfill";
 import bootstrapper from "public_site/bootstrapper";
 
-<<<<<<< HEAD
-=======
 
-if(!window.SENTRY_ENABLED) {
-    let sentryDSN = window.SENTRY_DSN;
-    Raven.config(sentryDSN, {
-        release: window.SENTRY_RELEASE
-    }).install();
-}
-
->>>>>>> a856735a7697a36e433d85ee5294bc58ce0f0d94
 bootstrapper.run();
